580f36a223e66e2b3b9e651a92ae00e61ee00a5c
Camille <caminie74@gmail.com>
2025-03-10 18:47:52 +0100

    ajout put/post/delete pour classroom et courses

--- json/json.php
+++ json/json.php
@@ -11,6 +11,8 @@ include "../crud/utilisateur.php";
 // boolean pour indiquer si les données ont été récupérées ou non
 $recupere=false;
 
+// SELECT avec la méthode GET
+
 if(isset($_GET['table']) && isset($_GET['type'])){
     
     $table=$_GET['table'];
@@ -140,14 +142,127 @@ if(isset($_GET['table']) && isset($_GET['type'])){
 
 
 
+header('Content-Type: application/json');
+
+// Vérifier la méthode HTTP de la requête
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case 'PUT': // pour les UPDATE
+
+        // Récupérer les données envoyées via PUT
+        $input = json_decode(file_get_contents('php://input'), true);
+
+        if (isset($input['type'])) {
+            $type = $input['type'];  // Type : classroom / courses
+
+            $modif=false;
+            // maj en fonction du type
+            switch ($type) {
+                case 'classroom': // Pour update_classroom
+                    if (isset($input['num']) && isset($input['specificity'])) {
+                        $num = $input['num'];
+                        $specificity = $input['specificity'];
+                        $modif = update_classroom($conn, $num, $specificity);
+                    }
+                    break;
+
+                case 'courses': // Pour les courses
+                    if (isset($input['id']) && isset($input['day']) && isset($input['date'])  && isset($input['recurrent'])  && isset($input['h_start'])  && isset($input['h_end'])  && isset($input['subject']['id'])  && isset($input['teacher']['id'])  && isset($input['class_name'])  && isset($input['classroom']['num']) ) {
+                        $id = $input['id'];
+                        $day = $input['day'];
+                        $date = $input['date'];
+                        $recurrent = $input['recurrent'];
+                        $h_start = $input['h_start'];
+                        $h_end = $input['h_end'];
+                        $subject_id = $input['subject']['id'];
+                        $teacher_id = $input['teacher']['id'];
+                        $class_name = $input['class_name'];
+                        $classroom_num = $input['classroom']['num'];
+                        $modif = update_courses($conn,$id, $day,$date,$recurrent,$h_start,$h_end,$subject_id,$teacher_id,$class_name,$classroom_num);
+                    }
+                    break;
+
+            }
+        }
+        break;
+    case 'POST': // INSERT
+        // Récupérer les données envoyées via POST pour les insertions
+        $input = json_decode(file_get_contents('php://input'), true);
+    
+        if (isset($input['type'])) {
+            $type = $input['type'];  // Type : classroom
+    
+            // insert en fonction du type
+            switch ($type) {
+                case 'classroom':
+                    if (isset($input['num']) && isset($input['specificity'])) {
+                        $num = $input['num'];
+                        $specificity = $input['specificity'];
+                        $modif =insert_classroom($conn, $num, $specificity);
+                    }
+                    break;
+                case 'courses':
+                    if(isset($input['day']) && isset($input['date']) && isset($input['recurrent']) && isset($input['h_start']) && isset($input['h_end']) &&  isset($input['subject_id']) && isset($input['teacher_id']) && isset($input['class_name']) && isset($input['classroom_num']) ){
+                        $day=$input['day'];
+                        $date=$input['date'];
+                        $recurrent=$input['recurrent'];
+                        $h_start=$input['h_start'];
+                        $h_end=$input['h_end'];
+                        $subject_id=$input['subject_id'];
+                        $teacher_id=$input['teacher_id'];
+                        $class_name=$input['class_name'];
+                        $classroom_num=$input['classroom_num'];
+                        $modif = insert_courses($conn,$day,$date,$recurrent,$h_start,$h_end,$subject_id,$teacher_id,$class_name,$classroom_num);
+                    }
+                    break;
+            }
+        }
+        break;
+    case 'DELETE':
+        // Récupérer les données envoyées via DELETE pour les suppressions
+        $input = json_decode(file_get_contents('php://input'), true);
+    
+        if (isset($input['type'])) {
+            $type = $input['type'];  // Type : classroom
+    
+            // Gérer la suppression en fonction du type
+            switch ($type) {
+                // CLASSROOM
+                case 'classroom':
+                    if (isset($input['num'])) {
+                        $num = $input['num'];
+                        $modif = delete_classroom($conn, $num);
+                    } 
+                    break;
+                // COURSES
+                case 'courses':
+                    if(isset($input['id'])){
+                        $id=$input['id'];
+                        $modif = delete_courses($conn,$id);
+                    }
+                    break;
+            }
+        }
+
+    }
+
+    if($modif){
+        $recupere=true;
+        $donnees="modification ok";
+    }
+
+
+
+
 if($recupere){
-    /* Transforme le tableau associatif PHP en chaine JSON bien formé*/
+    // Transforme le tableau associatif PHP en chaine JSON bien formé
     $donnees_str = json_encode($donnees);
 
-    /* Notifie la navigateur que le type de donnees est JSON*/
+    // Notifie la navigateur que le type de donnees est JSON
     header('Content-Type: application/json; charset=utf-8');
 
-    /* Ecrit les donnees au format JSON*/ 
+    // Ecrit les donnees au format JSON
     echo"${donnees_str}";
 }else{
    $donnees_str = json_encode("erreur");//("erreur");
@@ -156,5 +271,4 @@ if($recupere){
 } 
 
 
-
-    ?>
+?>
