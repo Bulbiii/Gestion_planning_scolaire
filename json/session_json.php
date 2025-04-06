69d7f64ecb2e39623dc9414f0d1e472779ed4a05
Camille <caminie74@gmail.com>
2025-04-06 11:36:30 +0200

    modification session_json.php

--- json/session_json.php
+++ json/session_json.php
@@ -3,12 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 session_start();
-$input = json_decode(file_get_contents('php://input'), true);
 
-if ($input && isset($input['name']) && isset($input['mail'])) {
-    $_SESSION['user']['name'] = $input['name'];
-    echo json_encode(['success' => true, 'message' => 'Session mise à jour']);
-}
 
 echo json_encode($_SESSION);
 
